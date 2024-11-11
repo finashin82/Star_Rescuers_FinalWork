dacde3da00139b90c1af154be58808f348346fa5
finashin82 <finashin82@mail.ru>
2024-11-11 16:36:26 +0900

    Готово
    Готово

--- Star_Rescuers_FinalWork/Assets/Scripts/OldMove/GlobalStringVars.cs
+++
@@ -1,9 +0,0 @@
-public class GlobalStringVars
-{
-    #region Movement
-    public const string _horizontalAxis = "Horizontal";
-    public const string _verticalAxis = "Vertical";
-    public const string _jumpButton = "Jump";
-    public const string _fire1 = "Fire1";
-    #endregion
-}
